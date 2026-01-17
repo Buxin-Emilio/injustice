71fc409d544bf31e4efa45d2f594675655d04ae3
Buxin-Emilo <zgsxiaohao@gmail.com>
2026-01-17 23:02:11 +0100

    0.04

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-document.getElementById("ok").innerHTML = 5 + 62;
-let person="Hege";
-alert(person);
+// document.getElementById("ok").innerHTML = 5 + 62;
+// let person="Hege";
+// alert(person);
 function myFunction() {
   document.getElementById("demo1").innerHTML = "Hello Dolly!";
   document.getElementById("demo2").innerHTML = "How are you?";
