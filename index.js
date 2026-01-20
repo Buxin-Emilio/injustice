c2343fddf1d4c94ecf06afed9ce1a5c4d6429660
Buxin-Emilo <zgsxiaohao@gmail.com>
2026-01-21 00:06:01 +0100

    0.05

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 // document.getElementById("ok").innerHTML = 5 + 62;
 // let person="Hege";
 // alert(person);
-function myFunction() {
-  document.getElementById("demo1").innerHTML = "Hello Dolly!";
-  document.getElementById("demo2").innerHTML = "How are you?";
-};
-myFunction();
+// function myFunction() {
+//   document.getElementById("demo1").innerHTML = "Hello Dolly!";
+//   document.getElementById("demo2").innerHTML = "How are you?";
+// };
+// myFunction();
+// let x = 10 ;
+// alert(x *10);
+// alert("john" +"  "+ x*10 + "ok");
+let x = "5" + 2 + 3;
+alert(x);
