92ac22a7b3b6cbb95e082db31512062c5efb6f22
Buxin-Emilo <zgsxiaohao@gmail.com>
2026-01-23 19:58:10 +0100

    0.06

--- index.js
+++ index.js
@@ -9,5 +9,20 @@
 // let x = 10 ;
 // alert(x *10);
 // alert("john" +"  "+ x*10 + "ok");
-let x = "5" + 2 + 3;
-alert(x);
+// let x = "5" + 2 + 3;
+// alert(x);
+// let a = "A", b = "a";
+// alert(a<b);
+// function Person(first, last, age, eye) {
+//   this.firstName = first;
+//   this.lastName = last;
+//   this.age = age;
+//   this.eyeColor = eye;
+// };
+// let moTher  = new Person("a" , "b" ,0 , "none");
+// moTher.changeName=function(name2) {
+// this.lastName = name2;
+// };
+// moTher.changeName("c");
+// alert(moTher.lastName)
+
